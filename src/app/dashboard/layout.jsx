4cfbb33ae6108445d25fb0f8bc3c9601995998da
Najmulkhan1 <najmulislam624@gmail.com>
2026-02-18 18:05:47 +0600

    some probelm are solve

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,14 +1,14 @@
-// যদি metadata থাকে, সেটা named export হবে
+
 export const metadata = {
   title: 'Appointment Dashboard',
   description: 'Manage your appointments',
 };
 
-// এই অংশটি মিসিং বা ভুল থাকার কারণেই এরর আসছে
+
 export default function DashboardLayout({ children }) {
   return (
     <section>
-      {/* এখানে আপনার লেআউটের ডিজাইন থাকতে পারে */}
+      {/* Layout design */}
       <div className="p-4">
         {children}
       </div>
