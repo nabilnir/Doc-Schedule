73d9c7ecc4c15ee07ab5ac3b86950c9cac56fb6d
Nabil Mahmud <nirarhan@gmail.com>
2026-02-26 06:29:48 +0600

    Add image hostname to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.pravatar.cc",
       },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com"
+      }
     ],
   },
   reactCompiler: true,
